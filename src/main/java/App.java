1c141ffbe57614485e14876bb75c0986d72a98bd
VladimirKovtun <vovkammmorovka@gmail.com>
2022-12-08 02:02:37 +0300

    Add new bean with scope prototype

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -7,6 +7,21 @@ public class App {
                 new AnnotationConfigApplicationContext(AppConfig.class);
         HelloWorld bean =
                 (HelloWorld) applicationContext.getBean("helloworld");
-        System.out.println(bean.getMessage());
+        HelloWorld bean2 = (HelloWorld) applicationContext.getBean("helloworld");
+        bean2.setMessage("Oops");
+        System.out.println("bean - " + bean.getMessage());
+        System.out.println("bean 2 - " + bean2.getMessage());
+        System.out.print(bean == bean2);
+        System.out.println("   <-  Singleton scope.");
+
+        System.out.println("------------------------");
+
+        Cat murzik = applicationContext.getBean("cat", Cat.class);
+        System.out.println("murzik says - " + murzik.getVoice());
+        Cat barsik = applicationContext.getBean("cat", Cat.class);
+        barsik.setVoice("Murrrrr");
+        System.out.println("barsik says - " + barsik.getVoice());
+        System.out.print(murzik == barsik);
+        System.out.print("   <-  Prototype scope.");
     }
 }
